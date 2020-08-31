3dd30c458534afc800bb1ed21a7556bae3301e64
zach-he <971499086@qq.com>
2020-08-31 20:46:39 +0800

    [zach]feature:init project

---
+++ src/main/java/com/zach/springbootcustomexceptiondemo/SpringbootCustomExceptionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zach.springbootcustomexceptiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomExceptionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCustomExceptionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zach/springbootcustomexceptiondemo/SpringbootCustomExceptionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.springbootcustomexceptiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomExceptionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
